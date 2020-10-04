9ef1fe6b3bf8180e759d0b305278fd50c15da0f5
Ben Marden <benvmarden@gmail.com>
2020-10-03 19:00:25 -0700

    Manage forecast state in App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import Axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import DailyForecast from './components/DailyForecast';
+import HourlyForecast from './components/HourlyForecast';
 import Navigation from './components/Navigation';
-import Search from './components/Search';
 
 export default () => {
   const [hourlyForecast, setHourlyForecast] = useState(null);
+  const [curWeather, setCurWeather] = useState(null);
+  const [searchText, setSearchText] = useState('Chico, CA, USA');
 
-  // useEffect(() => {
-  //   if (forecast != null) {
-  //     console.log(forecast);
-  //   }
-  // });
+  const handleSearch = (searchText) => {
+    setSearchText(searchText);
+    console.log(searchText);
+  };
+
+  useEffect(() => {
+    const getHourlyForecast = async () => {
+      try {
+        const response = Axios.get(
+          'https://api.openweathermap.org/data/2.5/forecast',
+          {
+            params: {
+              q: searchText,
+              units: 'imperial',
+              appid: process.env.REACT_APP_OW_API_KEY,
+            },
+          }
+        );
+        setHourlyForecast(response);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getHourlyForecast();
+  }, [searchText]);
+
   return (
     <div>
-      <Navigation setDailyForecast={setHourlyForecast} />
+      <Navigation onButtonClick={handleSearch} />
       <div>
-        <DailyForecast hourlyForecast={hourlyForecast} />
+        <HourlyForecast forecast={hourlyForecast} />
+        {/* <DailyForecast hourlyForecast={hourlyForecast} /> */}
       </div>
     </div>
   );
