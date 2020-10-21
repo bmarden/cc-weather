279f0caa87bb6f256b0a347e3ca00f527231a04e
Ben Marden <benvmarden@gmail.com>
2020-10-20 23:37:30 -0700

    Add redux dev tools, update .vscode setting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './App';
@@ -10,7 +10,8 @@ import reducers from './reducers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.scss';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
