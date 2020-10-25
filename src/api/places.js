b039d16341ea3363baf9a7ca7219f62b39060b17
Ben Marden <benvmarden@gmail.com>
2020-10-25 02:34:38 -0700

    Add Google Places Autocomplete API load function

--- src/api/places.js
+++ src/api/places.js
@@ -1,1 +1,19 @@
-<script src="https://maps.googleapis.com/maps/api/js?key=&libraries=places"></script>;
+export const loadGoogleMapsApi = (callback) => {
+  let script = document.createElement('script');
+  const url = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`;
+  script.type = 'text/javascript';
+
+  if (script.readyState) {
+    script.onreadystatechange = function () {
+      if (script.readyState === 'loaded' || script.readyState === 'complete') {
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {
+    script.onload = () => callback();
+  }
+
+  script.src = url;
+  document.getElementsByTagName('head')[0].appendChild(script);
+};
