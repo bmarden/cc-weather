4c263a046320b7f34aa59d42fa235dd2d22622ea
Ben Marden <benvmarden@gmail.com>
2020-10-25 02:35:13 -0700

    Add latitute longitude expansion function

--- src/common/utils.js
+++ src/common/utils.js
@@ -9,3 +9,14 @@ export const capitalizeFirstChar = (s) => {
   if (typeof s !== 'string') return '';
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
+
+// Expands a given lat, lon in 10 miles each direction
+// Adapted from https://gis.stackexchange.com/questions/15545/calculating-coordinates-of-square-x-miles-from-center-point
+export const expandLatLon = (coords) => {
+  const miles = 10;
+  const df = miles / 69;
+  const dl = df / Math.cos(coords.lat);
+  const swCoords = { lat: coords.lat - df, lon: coords.lon - dl };
+  const neCoords = { lat: coords.lat + df, lon: coords.lon + dl };
+  return [swCoords, neCoords];
+};
