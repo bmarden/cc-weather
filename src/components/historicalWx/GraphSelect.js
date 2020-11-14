8ba122a48153c46972896f0197bf625e6135b389
Ben Marden <benvmarden@gmail.com>
2020-11-14 03:20:16 -0800

    Add Nav and dropdown showing wx stations

--- src/components/historicalWx/GraphSelect.js
+++ src/components/historicalWx/GraphSelect.js
@@ -1,6 +1,16 @@
 import React, { useEffect } from 'react';
+import {
+  Card,
+  Dropdown,
+  Nav,
+  NavItem,
+  DropdownButton,
+  OverlayTrigger,
+  Tooltip,
+} from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchStationData, fetchHistTemp } from './histWxSlice';
+import Historical from '../historicalWx/Historical';
 
 const GraphSelect = () => {
   const dispatch = useDispatch();
@@ -19,7 +29,52 @@ const GraphSelect = () => {
     }
   }, [stations, dispatch]);
 
-  return <div>Graph Select</div>;
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Simple tooltip
+    </Tooltip>
+  );
+
+  const renderStations = () => {
+    if (stations) {
+      const items = stations.map((stn) => (
+        <OverlayTrigger
+          key={stn.uid}
+          placement="left"
+          delay={{ show: 250, hide: 400 }}
+          overlay={renderTooltip}
+        >
+          <Dropdown.Item key={stn.uid}>{stn.name.toLowerCase()}</Dropdown.Item>
+        </OverlayTrigger>
+      ));
+      return items;
+    }
+  };
+
+  return (
+    <Card>
+      <Card.Header>
+        <Nav variant="pills" defaultActiveKey="#daily">
+          <Nav.Item>
+            <Nav.Link href="#daily">By Day</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#monthly">By Month</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#yearly">By Year</Nav.Link>
+          </Nav.Item>
+          <DropdownButton className="ml-auto" title="stations" as={NavItem}>
+            {renderStations()}
+          </DropdownButton>
+        </Nav>
+      </Card.Header>
+      <Card.Body>
+        <Card.Title>Historical Data</Card.Title>
+        <Historical />
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default GraphSelect;
