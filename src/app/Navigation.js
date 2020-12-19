09c8c2817497255f91f5a299ee39728fa573c57b
Ben Marden <benvmarden@gmail.com>
2020-12-18 23:25:42 -0800

    Add logo to navbar

--- src/app/Navigation.js
+++ src/app/Navigation.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import ccweather from '../assets/cc-weather.png';
 
 const Navigation = () => {
   return (
     <Navbar collapseOnSelect bg="#000046" variant="dark" expand="lg">
       <Navbar.Brand as={NavLink} to="/">
-        CC-Weather
+        <img
+          src={ccweather}
+          className="d-inline-block align-top"
+          alt="cc-weather"
+          width="40"
+          height="40"
+        />
+      </Navbar.Brand>
+      <Navbar.Brand as={NavLink} to="/">
+        CC Weather
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
